92d92869be7fbfd0a053f61899864c30addff434
Michael <Mlcorrieri@gmail.com>
2021-04-07 17:11:47 -0400

    done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+function EyesOnMe() {
+  function consoleFocus() {
+    console.log("Good!");
+  }
+  function consoleBlur() {
+    console.log("Hey! Eyes on me!");
+  }
+
+  return (
+    <button onFocus={consoleFocus} onBlur={consoleBlur}>
+      Eyes on me
+    </button>
+  );
+}
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,13 @@
 // Code Keypad Component Here
+function Keypad() {
+  function consoleThis(event) {
+    console.log("Entering password...");
+  }
+
+  return (
+    <div>
+      <input onChange={consoleThis} type="password" />;
+    </div>
+  );
+}
+export default Keypad;
